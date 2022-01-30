2ffde3d548e10d09df44c648c267cc804dade221
Aarnav <aarnavbos@gmail.com>
2022-01-30 21:53:02 +0100

    init commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from main import Algorave
+
+
+def create_instance(c_instance):
+    return Algorave(c_instance)
---
+++ main.py
@@ -0,0 +1,6 @@
+from ableton.v2.control_surface import ControlSurface
+
+
+class Algorave(ControlSurface):
+    def __init__(self, c_instance, *args, **kwargs):
+        super().__init__(c_instance, args, kwargs)
